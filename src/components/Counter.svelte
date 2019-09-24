1cda2165758d407d3c0863977d11f8dd22977207
Fulvio <fulviocanducci@hotmail.com>
2019-09-24 14:46:27 -0300

    first

---
+++ src/components/Counter.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let counter = 0;
+	function handleIncrement() {
+		counter ++;
+	}
+	function handleDecrement() {
+		counter --;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Counter: {counter}</h1>
+
+<button on:click={handleDecrement}>Descrement</button>
+<button on:click={handleIncrement}>Increment</button>
---
+++ src/components/List.svelte
@@ -0,0 +1,16 @@
+<script>
+    let numbers = [];
+    const handleClick = () => {
+        numbers = [
+            ...numbers, Math.random().toFixed(2)
+        ];
+    }
+</script>
+
+<h1>List</h1>
+<button on:click={handleClick}>Add</button>
+<ul>
+    {#each numbers as item}
+        <li>{item}</li>
+    {/each}    
+</ul>
